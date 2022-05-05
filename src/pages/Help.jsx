f6feb713206d556b6a095a73c97a231cc26b027e
unknown <abuayubiqbal@gmail.com>
2022-05-05 20:13:13 +0600

    state and useEffect

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from 'react'
 import Layout from '../components/layout/Layout'
 
 const Help = () => {
+
+    const [state, setState] = useState({ name: "" })
+    useEffect(() => {
+        setTimeout(() => {
+            setState({ name: "Ayyub Iqbal" })
+            console.log('name changed');
+        }, 3000);
+        console.log('Rendering');
+    }, [])
+
+
+    const [name, setName] = useState("")
+    const data = [
+        {
+            name: "Ayyub Iqbal",
+            email: "ayyub@gmail.com",
+        },
+        {
+            name: "Muhammad Iqbal",
+            email: "muhammad@gmail.com",
+        },
+        {
+            name: "Sorif Iqbal",
+            email: "sorif@gmail.com",
+        },
+        {
+            name: "Kawsar Iqbal",
+            email: "kawsar@gmail.com",
+        },
+    ]
+    // const data = []
+    // console.log(data.length);
     return (
         <Layout>
-            <h1>This is Help page</h1>
+            {
+                state.name ? (<h1>Hello {state.name}, This is Help page</h1>) :
+                    (<h1>Hello Guest, This is Help page</h1>)
+            }
+
+            {
+                data.length > 0 ?
+                    <ul>
+                        {data.map((value) => <li>{value.name}, {value.email} </li>)}
+                    </ul> :
+                    <p>There is no data</p>
+
+            }
         </Layout>
     )
 }
